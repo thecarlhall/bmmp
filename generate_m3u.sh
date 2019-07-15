efc80aeb77548a685da3a0a745a761915ff945a0
🤦 Carl Hall 🤷 <carlhal@amazon.com>
2019-07-15 08:51:22 -0700

    Replace { } in playlist Change sed marker to comma

--- generate_m3u.sh
+++ generate_m3u.sh
@@ -19,17 +19,19 @@ fi
 # add a line for each mp3 and replace some url characters
 find $location -type f -iname '*.mp3' | \
     sed -e "s,^$location,$server,g" \
-        -e 's/ /%20/g' \
-        -e 's/!/%21/g' \
-        -e 's/"/%22/g' \
-        -e 's/#/%23/g' \
-        -e 's/\$/%24/g' \
-        -e 's/\&/%26/g' \
-        -e "s/'/%27/g" \
-        -e 's/(/%28/g' \
-        -e 's/)/%29/g' \
-        -e 's/\[/%5B/g' \
-        -e 's/\]/%5D/g' \
+        -e 's, ,%20,g' \
+        -e 's,!,%21,g' \
+        -e 's,",%22,g' \
+        -e 's,#,%23,g' \
+        -e 's,\$,%24,g' \
+        -e 's,\&,%26,g' \
+        -e "s,',%27,g" \
+        -e 's,(,%28,g' \
+        -e 's,),%29,g' \
+        -e 's,\[,%5B,g' \
+        -e 's,\],%5D,g' \
+        -e 's,{,%7B,g' \
+        -e 's,},%7D,g' \
     > "$output"
 
 line_count=$(sed -n '$=' "$output")
